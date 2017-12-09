7a099465812cd2ce5af1db3d4ef72f5625297adf
SHGroup <sunghun7511@naver.com>
2017-12-09 23:34:28 +0900

    Change some structure

--- src/com/SHGroup/JunmoGame/Models/CharacterObject.java
+++ src/com/SHGroup/JunmoGame/Models/CharacterObject.java
@@ -2,32 +2,10 @@ package com.SHGroup.JunmoGame.Models;
 
 import org.newdawn.slick.SlickException;
 
-public abstract class CharacterObject {
-	
-	private float x, y;
-	
-	public CharacterObject(float x, float y) throws SlickException{
-		this.x = x;
-		this.y = y;
-		
-		init();
+public abstract class CharacterObject extends ObjectModel{
+
+	public CharacterObject(float x, float y) throws SlickException {
+		super(x, y);
 	}
 	
-	public abstract void init() throws SlickException;
-	
-	public void setX(float x) {
-		this.x = x;
-	}
-	
-	public void setY(float y) {
-		this.y = y;
-	}
-	
-	public float getX() {
-		return x;
-	}
-	
-	public float getY() {
-		return y;
-	}
 }
